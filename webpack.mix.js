8b25f52dd5847222f3781facbfbb78294f267f26
Денис Гусельников <denis.guselnikov.2@gmail.com>
2023-04-16 20:58:07 +0300

    Первая страница. Разметка и стили

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,17 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .vue()
+    .sass('resources/sass/app.scss', 'public/css').version();
+    // version() - если запустим скрипт-сборщик, в папке public появится файл mix_manifest (будут указаны уникальные
+    // идентификаторы для каждого файла стилей и скриптов). Каждый раз при сборке webpack-ом данные файлового
+    // идентификатора будут менятся что не даст браузеру использовать файлы стилей или скриптов из кеша.
+
+mix.copy('resources/img', 'public/img');  // копирование папки (начальное и конечное место расположения)
+
